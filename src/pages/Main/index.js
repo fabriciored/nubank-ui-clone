610d30c82786223ca1a1f734baf7cc2df39b161e
Fabricio Santos <fabricio.lima@ykt.com.br>
2020-11-18 12:09:21 -0300

    Create Menu component

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,15 +1,43 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
+import Menu from '~/components/Menu';
 
-import { Container } from './styles';
+import { Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation } from './styles';
 
 export default function Main() {
   return (
     <Container>
       <Header/>
+
+      <Content>
+        <Menu />
+
+        <Card>
+
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666"/>
+            <Icon name="visibility-off" size={28} color="#666"/>
+          </CardHeader>
+
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 1.865,21</Description>
+          </CardContent>
+
+          <CardFooter>
+            <Annotation>
+              Transferencia de R$ 20,00 recebida de Fabrício Santos hojr as 11:00
+            </Annotation>
+          </CardFooter>
+
+        </Card>
+      </Content>
+
       <Tabs />
+
     </Container>
   )
 }
